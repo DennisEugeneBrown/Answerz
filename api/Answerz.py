ddebaaead17e7fd4ba0c41fae77d97dbac0ed247
Yousef <y.o.mansy@gmail.com>
2021-10-22 17:33:40 +0200

    Add react front-end

---
+++ api/Answerz.py
@@ -0,0 +1,35 @@
+import app
+from answerz_server import AnswerzProcessor
+from flask_restful import Resource, reqparse
+
+
+class Answerz(Resource):
+    def get(self):
+        return {
+            'resultStatus': 'SUCCESS',
+            'message': "Hello Answerz"
+        }
+
+    def post(self):
+        # return {"status": "Success", "message": {"Count_CallReportNum": 58113}}
+
+        ap = AnswerzProcessor(
+            app.app.config['DATAMAP'], app.app.config['DB'], app.app.config['LUIS'])
+        parser = reqparse.RequestParser()
+        parser.add_argument('text', type=str)
+        args = parser.parse_args()
+
+        text = args['text']
+        print(text)
+        results = ap.run_query(text)
+        sql_lower = []
+        out = []
+        for result, sql in results:
+            if sql.lower() in sql_lower:
+                continue
+            sql_lower.append(sql.lower())
+            out.append(result['Output'])
+            print(result['Output'])
+
+        final_ret = {"status": "Success", "message": out[0]}
+        return final_ret
---
+++ app.py
@@ -0,0 +1,38 @@
+import json
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS  # comment this on deployment
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app.config.from_json('config.json')
+
+CORS(app)  # comment this on deployment
+api = Api(app)
+
+
+@app.route("/", defaults={'path': ''})
+def serve(path):
+    print(app.static_folder)
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/columns", defaults={'path': ''})
+def get_columns(path):
+    columns = []
+    for dim in app.config['DATAMAP']['calls']['Dimensions']:
+        if isinstance(dim['field'], str):
+            columns.append(dim['field'].title())
+        else:
+            columns.extend([val.title() for val in dim['field'].values()])
+    return json.dumps(list(set(columns)))
+
+
+@app.route("/tables", defaults={'path': ''})
+def get_tables(path):
+    tables = [table.title() for table in app.config['DATAMAP'].keys()]
+    return json.dumps(list(set(tables)))
+
+
+from api.Answerz import Answerz
+
+api.add_resource(Answerz, '/answerz')
