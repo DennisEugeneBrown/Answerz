df9ed074f3f1f90223938da25a30bda69455d79b
Yousef <y.o.mansy@gmail.com>
2022-01-11 08:25:16 +0200

    Jan 11 Session - Categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -77,7 +77,7 @@ function App() {
     const [weight, setWeight] = React.useState('');
 
     const [getPrevQuery, setPrevQuery] = useState('')
-    const versionNumber = '2.0.31'
+    const versionNumber = '2.0.32'
 
     const [getInputState, setInputState] = useState('');
 
