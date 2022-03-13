395a5b55dadebcfb784a8a0ec8c775778ad4ac8b
Yousef <y.o.mansy@gmail.com>
2022-03-13 02:02:48 +0200

    Frontend update

--- answerz/api/__init__.py
+++ answerz/api/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS  # comment this on deployment
 
 
 def create_app():
-    app = Flask(__name__, static_url_path='', static_folder=os.path.join('../view/build'))
+    app = Flask(__name__, static_url_path='', static_folder=os.path.join('../view_OLD/build'))
     app.config.from_json('../config.json')
 
     CORS(app)  # comment this on deployment
